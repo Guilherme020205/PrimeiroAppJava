13521ba33fd5b6112e9d2602aabd1ce7a65037b8
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2025-02-19 22:05:43 -0300

    Finalizada a aula aonde foi criado um app que permite sortear um numero inteiro de 0 a 10 exibir na tela mechendo com o status do text e onCLick com botão

--- app/src/main/java/com/cursoandroid/primeiroapp/MainActivity.java
+++ app/src/main/java/com/cursoandroid/primeiroapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.cursoandroid.primeiroapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,4 +25,20 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void sorteioNumber(View view){
+
+        TextView textResult = findViewById(R.id.textView5);
+
+        int numero = new Random().nextInt(11);
+
+        textResult.setText("Número sorteado é: " + numero);
+
+    }
+
+    public void recomecar(View view){
+        TextView textResult = findViewById(R.id.textView5);
+        textResult.setText("Nenhum número selecionado");
+    }
+
 }
